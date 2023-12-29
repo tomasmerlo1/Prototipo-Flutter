dd4555e2f78b931cfb7e116589fd73f6cadae00a
sofigonzalez14 <sofimacag14@gmail.com>
2023-12-29 18:12:35 -0300

    button segunda pantalla

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:aplication_noticias/screens/home_screen.dart';
-// ignore: unused_import
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'Demo',
       home: HomeScreen(),
     );
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,61 +1,55 @@
+import 'package:aplication_noticias/screens/listviewregister_screen.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Home Screen"),
+        title: Text("INICIO"),
         centerTitle: true,
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Agrega la imagen
-          Image.asset(
-            'assets/imagenes/noticiero.jpg', 
-            width: 200,
-            height: 200,
-            fit: BoxFit.cover,
-          ),
-          SizedBox(height: 16),
-          // Agrega el título
-          Text(
-            'BAHIA NOTICIAS',
-            style: TextStyle(
-              fontSize: 24,
-              fontWeight: FontWeight.bold,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Image.asset(
+              'imagenes/noticiero.jpg',
+              width: 300,
+              height: 300,
+              fit: BoxFit.cover,
             ),
-          ),
-          SizedBox(height: 8),
-          // Agrega la breve descripción
-          Text(
-            'Enterate de todas las noticias del dia a dia en Bahia Blanca',
-            style: TextStyle(
-              fontSize: 16,
+            SizedBox(height: 18),
+            Text(
+              'BAHIA NOTICIAS',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
             ),
-            textAlign: TextAlign.center,
-),
-        SizedBox(height: 16),
-          // Contenido original
-          const Center(
-            child: Text('Widget Original'),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          print('hice click en el boton');
-        },
-        child: const Icon(
-          Icons.add,
-          size: 30,
+            SizedBox(height: 8),
+            Text(
+              'Enterate de todas las noticias del día a día en Bahía Blanca',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Navegar a la segunda pantalla al hacer clic en el botón
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ListViewScreen()),
+                );
+              },
+              child: Text('Ir a Noticias'),
+            ),
+          ],
         ),
       ),
-      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
     );
   }
 }
