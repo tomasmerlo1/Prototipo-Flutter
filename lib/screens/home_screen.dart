b43efbb1ef9254926867f6f561beddf590238c6a
MacaHuarte09 <macaahuarte09@gmail.com>
2024-01-03 10:08:30 -0300

    vuelta atras

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -50,32 +50,6 @@ class HomeScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              SizedBox(height: 1.5),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => Noticias(),
-                    ),
-                  );
-                  },
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.all<Color>(Color.fromARGB(237, 238, 239, 255)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                      RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(18.0),
-                      ),
-                    ),
-                  ),
-                  child: Text(
-                    'Noticia del día',
-                    style: TextStyle(
-                      fontSize: 18,
-                      color: Colors.blue,
-                  ),
-                ),
-              )
             ],
           ),
         ),
